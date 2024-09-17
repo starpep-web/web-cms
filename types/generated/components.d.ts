ef6cec90257649549c3b453ecb14f125329bdcff
moonstar-x <christian.lopez99@outlook.com>
2024-09-16 23:17:16 -0500

    Added new color to TextCard.

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,57 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface CommonTextImageColumn extends Schema.Component {
+  collectionName: 'components_common_text_image_columns';
+  info: {
+    displayName: 'TextImageColumn';
+    icon: 'layout';
+    description: '';
+  };
+  attributes: {
+    text: Attribute.RichText & Attribute.Required;
+    image: Attribute.Media<'images'> & Attribute.Required;
+    flip: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<false>;
+  };
+}
+
+export interface CommonTextCard extends Schema.Component {
+  collectionName: 'components_common_text_cards';
+  info: {
+    displayName: 'TextCard';
+    icon: 'strikeThrough';
+    description: '';
+  };
+  attributes: {
+    text: Attribute.RichText & Attribute.Required;
+    color: Attribute.Enumeration<['Secondary', 'Dark']> &
+      Attribute.Required &
+      Attribute.DefaultTo<'Secondary'>;
+  };
+}
+
+export interface CommonImageGallery extends Schema.Component {
+  collectionName: 'components_common_image_galleries';
+  info: {
+    displayName: 'ImageGallery';
+    icon: 'landscape';
+  };
+  attributes: {
+    images: Attribute.Media<'images', true> & Attribute.Required;
+  };
+}
+
+export interface CommonFaq extends Schema.Component {
+  collectionName: 'components_common_faqs';
+  info: {
+    displayName: 'FAQ';
+    icon: 'question';
+  };
+  attributes: {
+    question: Attribute.Text & Attribute.Required;
+    answer: Attribute.RichText & Attribute.Required;
+  };
+}
+
 export interface TeamTeamMemberCard extends Schema.Component {
   collectionName: 'components_team_team_member_cards';
   info: {
@@ -34,66 +86,15 @@ export interface TeamTeamMemberCardGroup extends Schema.Component {
   };
 }
 
-export interface CommonTextImageColumn extends Schema.Component {
-  collectionName: 'components_common_text_image_columns';
-  info: {
-    displayName: 'TextImageColumn';
-    icon: 'layout';
-    description: '';
-  };
-  attributes: {
-    text: Attribute.RichText & Attribute.Required;
-    image: Attribute.Media<'images'> & Attribute.Required;
-    flip: Attribute.Boolean & Attribute.Required & Attribute.DefaultTo<false>;
-  };
-}
-
-export interface CommonTextCard extends Schema.Component {
-  collectionName: 'components_common_text_cards';
-  info: {
-    displayName: 'TextCard';
-    icon: 'strikeThrough';
-  };
-  attributes: {
-    text: Attribute.RichText & Attribute.Required;
-    color: Attribute.Enumeration<['Secondary']> &
-      Attribute.Required &
-      Attribute.DefaultTo<'Secondary'>;
-  };
-}
-
-export interface CommonImageGallery extends Schema.Component {
-  collectionName: 'components_common_image_galleries';
-  info: {
-    displayName: 'ImageGallery';
-    icon: 'landscape';
-  };
-  attributes: {
-    images: Attribute.Media<'images', true> & Attribute.Required;
-  };
-}
-
-export interface CommonFaq extends Schema.Component {
-  collectionName: 'components_common_faqs';
-  info: {
-    displayName: 'FAQ';
-    icon: 'question';
-  };
-  attributes: {
-    question: Attribute.Text & Attribute.Required;
-    answer: Attribute.RichText & Attribute.Required;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
-      'team.team-member-card': TeamTeamMemberCard;
-      'team.team-member-card-group': TeamTeamMemberCardGroup;
       'common.text-image-column': CommonTextImageColumn;
       'common.text-card': CommonTextCard;
       'common.image-gallery': CommonImageGallery;
       'common.faq': CommonFaq;
+      'team.team-member-card': TeamTeamMemberCard;
+      'team.team-member-card-group': TeamTeamMemberCardGroup;
     }
   }
 }
