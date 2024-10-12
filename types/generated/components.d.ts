b7d12b0eeb1b7690b9e465a30a3498fe2180bfd4
moonstar-x <christian.lopez99@outlook.com>
2024-10-12 00:44:45 -0500

    Generated update.

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,5 +1,39 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface TeamTeamMemberCard extends Schema.Component {
+  collectionName: 'components_team_team_member_cards';
+  info: {
+    displayName: 'TeamMemberCard';
+    icon: 'user';
+  };
+  attributes: {
+    avatar: Attribute.Media<'images'>;
+    name: Attribute.String & Attribute.Required;
+    email: Attribute.Email & Attribute.Required;
+    occupation: Attribute.String;
+    affiliations: Attribute.Text;
+    website: Attribute.String;
+    location: Attribute.String;
+    shortCv: Attribute.RichText;
+  };
+}
+
+export interface TeamTeamMemberCardGroup extends Schema.Component {
+  collectionName: 'components_team_team_member_card_groups';
+  info: {
+    displayName: 'TeamMemberCardGroup';
+    icon: 'folder';
+  };
+  attributes: {
+    title: Attribute.String & Attribute.Required;
+    type: Attribute.Enumeration<['Featured', 'Simple']> &
+      Attribute.Required &
+      Attribute.DefaultTo<'Simple'>;
+    members: Attribute.Component<'team.team-member-card', true> &
+      Attribute.Required;
+  };
+}
+
 export interface CommonTextImageColumn extends Schema.Component {
   collectionName: 'components_common_text_image_columns';
   info: {
@@ -52,49 +86,15 @@ export interface CommonFaq extends Schema.Component {
   };
 }
 
-export interface TeamTeamMemberCard extends Schema.Component {
-  collectionName: 'components_team_team_member_cards';
-  info: {
-    displayName: 'TeamMemberCard';
-    icon: 'user';
-  };
-  attributes: {
-    avatar: Attribute.Media<'images'>;
-    name: Attribute.String & Attribute.Required;
-    email: Attribute.Email & Attribute.Required;
-    occupation: Attribute.String;
-    affiliations: Attribute.Text;
-    website: Attribute.String;
-    location: Attribute.String;
-    shortCv: Attribute.RichText;
-  };
-}
-
-export interface TeamTeamMemberCardGroup extends Schema.Component {
-  collectionName: 'components_team_team_member_card_groups';
-  info: {
-    displayName: 'TeamMemberCardGroup';
-    icon: 'folder';
-  };
-  attributes: {
-    title: Attribute.String & Attribute.Required;
-    type: Attribute.Enumeration<['Featured', 'Simple']> &
-      Attribute.Required &
-      Attribute.DefaultTo<'Simple'>;
-    members: Attribute.Component<'team.team-member-card', true> &
-      Attribute.Required;
-  };
-}
-
 declare module '@strapi/types' {
   export module Shared {
     export interface Components {
+      'team.team-member-card': TeamTeamMemberCard;
+      'team.team-member-card-group': TeamTeamMemberCardGroup;
       'common.text-image-column': CommonTextImageColumn;
       'common.text-card': CommonTextCard;
       'common.image-gallery': CommonImageGallery;
       'common.faq': CommonFaq;
-      'team.team-member-card': TeamTeamMemberCard;
-      'team.team-member-card-group': TeamTeamMemberCardGroup;
     }
   }
 }
